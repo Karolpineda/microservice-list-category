f1d8792e075547abe9f17ccb577b42be4c664a78
Jimmy9812 <wladimirmaila1998pm@gmail.com>
2025-02-05 01:38:54 -0500

    feat: :sparkles: update microservicelist

--- src/main/java/com/uce/microservicelistcategory/MicroserviceListCategoryApplication.java
+++ src/main/java/com/uce/microservicelistcategory/MicroserviceListCategoryApplication.java
@@ -1,12 +1,12 @@
-package com.uce.microserviceupdatecategory;
+package com.uce.microservicelistcategory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroserviceUpdateCategoryApplication {
+public class MicroserviceListCategoryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MicroserviceUpdateCategoryApplication.class, args);
+        SpringApplication.run(MicroserviceListCategoryApplication.class, args);
     }
 
 }
--- src/main/java/com/uce/microservicelistcategory/repository/microservicelistcategoryRepository.java
+++ src/main/java/com/uce/microservicelistcategory/repository/microservicelistcategoryRepository.java
@@ -1,11 +1,11 @@
-package com.uce.microserviceupdatecategory.repository;
+package com.uce.microservicelistcategory.repository;
 
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.uce.microserviceupdatecategory.model.Category;
+import com.uce.microservicelistcategory.model.Category;
 
-public interface microservicecreatecategoryRepository extends JpaRepository<Category, UUID> {
+public interface microservicelistcategoryRepository extends JpaRepository<Category, UUID> {
 }
 
